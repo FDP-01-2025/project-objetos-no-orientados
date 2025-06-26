e4f4a79ab65d6bf79390637867470f30ca7f71dd
Will1806 <00041625@uca.edu.sv>
2025-06-26 08:42:05 -0600

    process of messy word

--- src/messyWord/messyWord.h
+++ src/messyWord/messyWord.h
@@ -12,29 +12,29 @@
 
 using namespace std;
 
-string name1 = "Player 1";
-string name2 = "Player 2";
+extern string player_1;
+extern string player_2;
 
 // Inicializa la semilla aleatoria (hace que genere las palabras aleatoriamente)
-inline void inicializarAleatorio() {
+inline void InicializarAleatorio() {
     srand(static_cast<unsigned>(time(0)));
 }
 
 // Devuelve una lista de palabras
-inline vector<string> obtenerListaPalabras() {
+inline vector<string> ObtenerListaPalabras() {
     return {
         "computadora", "pantalla", "raton", "teclado", "codigo", "programa", "tecnologia"
     };
 }
 
 // Selecciona una palabra al azar
-inline string seleccionarPalabraAzar() {
-    vector<string> palabras = obtenerListaPalabras();
+inline string SeleccionarPalabraAzar() {
+    vector<string> palabras = ObtenerListaPalabras();
     return palabras[rand() % palabras.size()];
 }
 
 // Mezcla una palabra
-inline string mezclarPalabra(const string& palabra) {
+inline string MezclarPalabra(const string& palabra) {
     string mezclada = palabra;
     //Random_shuffle sirve para la mezcla de palabras de diferente forma
     random_shuffle(mezclada.begin(), mezclada.end());
@@ -42,101 +42,101 @@ inline string mezclarPalabra(const string& palabra) {
 }
 
 // Compara si es correcta
-inline bool comparar(const string& intento, const string& correcta) {
+inline bool Comparar(const string& intento, const string& correcta) {
     return intento == correcta;
 }
 
 // Simula si la CPU acierta (50% de probabilidad)
-inline bool cpuRespondeBien() {
+inline bool CpuRespondeBien() {
     return rand() % 2 == 0; // 0 o 1 → 50% de probabilidad
 }
 
 // Modo un jugador contra la CPU
-inline void modoUnJugador() {
-    int rondas = 3, puntosJugador = 0, puntosCPU = 0;
+inline void ModoUnJugador() {
+    int rondas = 5, puntosJugador = 0, puntosCPU = 0;
 
     for (int i = 1; i <= rondas; ++i) {
-        string palabra = seleccionarPalabraAzar();
-        string mezclada = mezclarPalabra(palabra);
+        string palabra = SeleccionarPalabraAzar();
+        string mezclada = MezclarPalabra(palabra);
         string intento;
 
-        cout << "\n[Ronda " << i << "] : Palabra desordenada: " << mezclada << "\n";
-        cout << "Tu intento: ";
+        cout << "\n[Ronda " << i << "] :\nPalabra desordenada: " << mezclada << "\n";
+        cout << player_1<< " Tu intento: ";
         cin >> intento;
 
-        if (comparar(intento, palabra)) {
-            cout << "---Correcto---\n";
+        if (Comparar(intento, palabra)) {
+            cout << "--Correcto--\n";
             ++puntosJugador;
         } else {
-            cout << "---Incorrecto. Era: " << palabra << " ---\n";
+            cout << "--Incorrecto. Era: " << palabra << " --\n";
         }
 
         // Turno de la CPU
-        cout << "CPU esta resolviendo...\n";
-        if (cpuRespondeBien()) {
-            cout << "CPU: " << palabra << " ---Correcto---\n";
+        cout << "\nCPU esta resolviendo...\n";
+        if (CpuRespondeBien()) {
+            cout << "CPU: " << palabra << " --Correcto--\n";
             ++puntosCPU;
         } else {
-            cout << "---CPU: respuesta incorrecta.---\n";
+            cout << "--CPU: respuesta incorrecta.--\n";
         }
     }
 
-    cout << "\nPuntaje Final:\nJugador: " << puntosJugador << "\nCPU: " << puntosCPU << "\n";
+    cout << "\n[(Puntaje Final)]:\n"<<player_1<<" "<< puntosJugador << "\nCPU: " << puntosCPU << "\n";
 
     if (puntosJugador > puntosCPU)
-        cout << "---Ganaste contra la CPU---\n";
+        cout << "--Ganaste contra la CPU--\n";
     else if (puntosCPU > puntosJugador)
-        cout << "---La CPU gana esta vez.---\n";
+        cout << "--La CPU gana esta vez.--\n";
     else
-        cout << "---Empate---\n";
+        cout << "--Empate--\n";
 }
 
 // Modo para dos jugadores 
-inline void modoMultijugador() {
+inline void ModoMultijugador() {
     int rondas = 3, puntos1 = 0, puntos2 = 0;
 
     for (int i = 1; i <= rondas; ++i) {
         // Jugador 1
-        string palabra1 = seleccionarPalabraAzar();
-        string mezclada1 = mezclarPalabra(palabra1);
+        string palabra1 = SeleccionarPalabraAzar();
+        string mezclada1 = MezclarPalabra(palabra1);
         string intento1;
 
-        cout << "\n[Ronda " << i << "] Jugador 1 - Palabra: " << mezclada1 << "\n";
+        cout << "\n[Ronda " << i << "]\nJugador "<<player_1<<" - Palabra: " << mezclada1 << "\n";
         cout << "Intento: ";
         cin >> intento1;
 
-        if (comparar(intento1, palabra1)) {
-            cout << "---Correcto---\n";
+        if (Comparar(intento1, palabra1)) {
+            cout << "--Correcto--\n";
             ++puntos1;
         } else {
-            cout << "---Incorrecto. Era: " << palabra1 << " ---\n";
+            cout << "--Incorrecto. Era: " << palabra1 << " --\n";
         }
 
         // Jugador 2
-        string palabra2 = seleccionarPalabraAzar();
-        string mezclada2 = mezclarPalabra(palabra2);
+        string palabra2 = SeleccionarPalabraAzar();
+        string mezclada2 = MezclarPalabra(palabra2);
         string intento2;
 
-        cout << "\n[Ronda " << i << "] Jugador 2 - Palabra: " << mezclada2 << "\n";
+        cout << "\n[Ronda " << i << "]\nJugador "<<player_2<<" - Palabra: " << mezclada2 << "\n";
         cout << "Intento: ";
         cin >> intento2;
 
-        if (comparar(intento2, palabra2)) {
-            cout << "---Correcto---\n";
+        if (Comparar(intento2, palabra2)) {
+            cout << "--Correcto--\n";
             ++puntos2;
         } else {
-            cout << "---Incorrecto. Era: " << palabra2 << " ---\n";
+            cout << "--Incorrecto. Era: " << palabra2 << " --\n";
         }
     }
 
-    cout << "\nPuntajes Finales:\n"<< name1 << " : " << puntos1 << "\n"<< name2 <<" : "<< puntos2 << "\n";
+    cout << "\n[(Puntajes Finales)]:\n"<< player_1<< " : " << puntos1 << "\n"<< player_2 <<" : "<< puntos2 << "\n";
 
     if (puntos1 > puntos2)
-        cout << "---Jugador "<< name1 <<" gana---";
+        cout << "--Jugador "<< player_1 <<" gana--";
     else if (puntos2 > puntos1)
-        cout << "---Jugador "<< name2 <<" gana---";
+        cout << "--Jugador "<< player_2 <<" gana--";
     else
-        cout << "---Empate---\n";
+        cout << "--Empate--\n";
 }
 
 #endif
